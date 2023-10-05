1024c90f6048ba64bfa940ef585ce0fd455a7635
Opeyemi1122 <alabiopex@gmail.com>
2023-10-05 14:05:08 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point of the program
+ *
+ * The program prints a line using the printf function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char grammar1 = 'A';
+
+	printf("with proper grammar, but the outcome is a piece of art,%d\n", grammar1);
+	return (0);
+}
