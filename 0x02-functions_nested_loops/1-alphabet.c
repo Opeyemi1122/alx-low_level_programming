6b599424fa7eab594ba9042dcd298eb44b21d6c6
Opeyemi1122 <alabiopex@gmail.com>
2023-10-10 05:48:18 -0700

    1 alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *main - prints alphabet in lowercase then new line
+ *print_alphabet - prints alphabet in lowercase then new line
  *
- *Return: Always 0.
+ *Return: Always 0
  */
 
 void print_alphabet(void)
