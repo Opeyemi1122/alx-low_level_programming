acc8b48e78b418193f19f4f54db3104d393dd029
Opeyemi1122 <alabiopex@gmail.com>
2023-10-14 11:51:00 -0700

    more numbers 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ * followed by a new line
+ * Return: 0.
+ */
+
+void more_numbers(void)
+{
+	int i;
+	if (i = 0; i <= 15; i++)
+		_putchar(i);
+
+	return (0);
+}
