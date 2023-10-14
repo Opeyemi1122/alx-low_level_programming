147dd097b552c4d7d55042d5d33e33a7afe14eff
Opeyemi1122 <alabiopex@gmail.com>
2023-10-13 17:20:45 -0700

    isdigit1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for character is a digit
+ * @c: numbers to check
+ * Return: 1 if c is uppercase
+ * Return 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
