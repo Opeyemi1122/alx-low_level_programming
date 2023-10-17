19e47b0276f72910d16fdaec4d7f90471855341b
Opeyemi1122 <alabiopex@gmail.com>
2023-10-16 17:52:37 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string, followed by a new line to stdout
+ * @str:new string to be printed
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+	_putchar(*str);
+	}
+	_putchar('\n');
+}
